6aecd4067487ce12071a4494670623fa26fde971
Milad-Shirvan <miladmagic3@gmail.com>
2025-05-15 08:50:18 +0200

    Just a simple JS
    just simple JS

---
+++ script.js
@@ -0,0 +1,40 @@
+let display = document.getElementById('display');
+
+function appendToDisplay(value) {
+    display.value += value;
+}
+
+function clearDisplay() {
+    display.value = '';
+}
+
+function deleteLast() {
+    display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value.replace('÷', '/').replace('×', '*'));
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
+
+document.addEventListener('keydown', (event) => {
+    const key = event.key;
+    if (/[0-9]/.test(key)) {
+        appendToDisplay(key);
+    } else if (key === '+' || key === '-' || key === '*' || key === '/') {
+        appendToDisplay(key);
+    } else if (key === 'Enter') {
+        calculate();
+    } else if (key === 'Backspace') {
+        deleteLast();
+    } else if (key === 'Escape') {
+        clearDisplay();
+    } else if (key === '.') {
+        appendToDisplay('.');
+    } else if (key === '(' || key === ')') {
+        appendToDisplay(key);
+    }
+});
